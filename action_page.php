05304ab1eb669d4d350b6714f12dc58a96369cde
Om Gupta <73100677+OMGUPTA21@users.noreply.github.com>
2021-10-28 19:31:15 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style20.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<!--menu bar-->
+<section>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div>
+       
+        <a href="#"><i class="fa fa-home" style="font-size:36px;color:red"></i></a>&nbsp;&nbsp;&nbsp;
+         <a href="#">SERVICE</a>&nbsp;&nbsp;&nbsp;
+        <a href="#">SUPPORT</a>&nbsp;&nbsp;&nbsp;
+        <a href="#">CONTACT</a>&nbsp;&nbsp;&nbsp;
+        <a href="#">MONTHLY PLAN</a>&nbsp;&nbsp;&nbsp;
+        <a href="login21.php"  target="_blank">SIGNUP</a>&nbsp;&nbsp;&nbsp;
+        <a href="#"><i class="fa fa-envelope" style="font-size:24px;color:red"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="signup.php"  target="_blank" class="log21" id= >LOGIN</a>
+        
+                  
+    </div>
+
+  </nav> 
+</section>
+<br>
+<center><img src="channels4_profile.jpg" alt="logo" class="edu"><b>&nbsp;GUPTA EDUCATIONAL HUB</b></center>
+
+
+  <!--OUR SERVICE-->
+  <section> 
+
+  <div class="container-fluid mb-5">
+    <div class="text-center mt-5">
+        <br>
+        <h1>Our Services</h1>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services settings">
+                    <div class="icon"> <img src="https://i.imgur.com/6NKPrhO.png"> </div>
+                    <h4>ONLINE BOOKS PDF</h4>
+                    <p>An online book is a resource in book-like form that is only available to read on the Internet.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services speedup">
+                    <div class="icon"> <img src="https://i.imgur.com/KMbnpFF.png"> </div>
+                    <h4>CODING VEDIO LECTURE</h4>
+                    <p>Computers and learning the basics of coding helps children to develop an appreciation of how things work.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services privacy">
+                    <div class="icon"> <img src="https://i.imgur.com/AgyneKA.png"> </div>
+                    <h4>INTERVIEW PEPRATION</h4>
+                    <p>Preapare Your shelf for a interview with us. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services backups">
+                    <div class="icon"> <img src="https://i.imgur.com/vdH9LKi.png"> </div>
+                    <h4>PARTNERSIP</h4>
+                    <p>Thank GeekforGeek to be your community partner. </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services ssl">
+                    <div class="icon"> <img src="https://i.imgur.com/v6OnUqu.png"> </div>
+                    <h4>SCOLARSHIP ALERT</h4>
+                    <p>WE inform about the latest scolarship for you.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="box">
+                <div class="our-services database">
+                    <div class="icon"> <img src="https://i.imgur.com/VzjZw9M.png"> </div>
+                    <h4>TIPS BY EXPERT</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+                <br>
+            </div>
+        </div>
+    </div>
+</div>
+  </section>
+<!--SUPPORT-->
+  <section >
+  <h1>SUPPORT</h1>
+  <div class='container-fluid mx-auto mt-5 mb-5 col-12' style="text-align: center">
+   
+    <div class="hd">Why People Believe in Us</div>
+    <p>Always render more and better service than <br />is expected of you, no matter what your ask may be.</small></p>
+    <div class="row" style="justify-content: center">
+        <div class="card col-md-3 col-12">
+            <div class="card-content">
+                <div class="card-body"> <img class="img" src="https://i.imgur.com/S7FJza5.png" />
+                    <div class="shadow"></div>
+                    <div class="card-title"> We're Free </div>
+                    <div class="card-subtitle">
+                        <p> We spent thousands of hours creating on algorithm that does this for you in seconds. We collect a small fee from the professional after they meet your</small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-md-3 col-12 ml-2">
+            <div class="card-content">
+                <div class="card-body"> <img class="img" src="https://i.imgur.com/xUWJuHB.png" />
+                    <div class="card-title"> We're Unbiased </div>
+                    <div class="card-subtitle">
+                        <p>We don't accept ads from anyone. We use actual data to match you who the best person for each job </small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-md-3 col-12 ml-2">
+            <div class="card-content">
+                <div class="card-body"> <img class="img rck" src="https://i.imgur.com/rG3CGn3.png" />
+                    <div class="card-title"> We Guide you </div>
+                    <div class="card-subtitle">
+                        <p> <small class="text-muted">Buying or selling a home is often the largest transaction anyone does in their life. we guide you through the process so that you can be confident in reaching your dream outcome.</small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ft">
+        <p class="chk"><small class="text-muted">Still not sure?</small></p>
+        <div class="btn btn-primary">Read All Testimonials</div>
+    <br>
+    <br>
+       
+       
+
+    </div>
+</div>
+  </section>
+  <!--contact us-->
+  <section >
+
+  <h1>CONTACT US</h1>
+  <center><div class="hd">Our team is always ready to help you.</div></center>
+
+  <div class="container">
+  
+    <div class="form">
+        <div class="contact-info">
+            <h3 class="title">Let's get in touch</h3>
+            <p class="text"> Contact us with the following details. and fillup the form with the details. </p>
+            <div class="info">
+                <div class="social-information"> <i class="fa fa-map-marker"></i>
+                    <p>suratkal,karnataka,INDIA</p>
+                </div>
+                <div class="social-information"> <i class="fa fa-envelope-o"></i>
+                    <p>support@edureaka.com</p>
+                </div>
+                <div class="social-information"> <i class="fa fa-mobile-phone"></i>
+                    <p>+1 989 989 9898 </p>
+                </div>
+            </div>
+            <div class="social-media">
+                <p>Connect with us :</p>
+                <div class="social-icons"> <a href="#"> <i class="fa fa-facebook-f" ></i> </a> <a href="#"> <i class="fa fa-twitter"></i> </a> <a href="#"> <i class="fa fa-instagram"></i> </a> <a href="#"> <i class="fa fa-linkedin"></i> </a> </div>
+            </div>
+        </div>
+        <div class="contact-info-form"> <span class="circle one"></span> <span class="circle two"></span>
+            <form action="#" onclick="return false;" autocomplete="off">
+                <h3 class="title">Contact us</h3>
+                <div class="social-input-containers"> <input type="text" name="name" class="input" placeholder="Name" /> </div>
+                <div class="social-input-containers"> <input type="email" name="email" class="input" placeholder="Email" /> </div>
+                <div class="social-input-containers"> <input type="tel" name="phone" class="input" placeholder="Phone" /> </div>
+                <div class="social-input-containers textarea"> <textarea name="message" class="input" placeholder="Message"></textarea> </div> <input type="submit" value="Send" class="btn" />
+            </form>
+            <br>
+
+            
+
+        </div>
+    </div>
+   <br>
+
+
+</section>
+<br>
+<br>
+<!--monthly plan-->
+
+<section>
+    <br>
+    <h1>MONTHLY PLAN</h1>
+    <center><div class="hd">YOU CAN CHEAKOUT OUR MONTHLY PLAN.</div></center>
+     
+     
+    <div class="container mt-5 mb-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4 mt-1">
+                <div class="part1 text-center p-3 py-3">
+                    <h6 class="free mt-4">FREE</h6> <span class=" amount mt-4 d-block">$0</span> <span class="user d-block mt-2">per user,per month</span> <span class="information d-block mt-4">1-10</span> <span class="information d-block mt-4">Available</span> <span class="information d-block mt-5">----</span> <span class="information d-block mt-4">Community</span>
+                </div>
+            </div>
+            <div class="col-md-4 mt-1">
+                <div class="part2 text-center p-3 py-3 text-white">
+                    <h6 class="free mt-4">POPULAR</h6> <span class=" amount mt-4 d-block">$8.50</span> <span class="user d-block mt-2">per user,per month</span> <span class="information d-block mt-4">11+</span> <span class="information d-block mt-4">Available</span> <span class="information d-block mt-5">Observers</span> <span class="information d-block mt-4">Priority</span>
+                </div>
+            </div>
+            <div class="col-md-4 mt-1">
+                <div class="part3 p-3 text-dark text-center py-3">
+                    <h6 class="free mt-4">ENTERPRISE</h6> <span class=" amount mt-4 d-block">CUSTOM</span> <span class="user d-block mt-2">include volume discounts</span> <span class="information d-block mt-4">50+</span> <span class="information d-block mt-4">Available</span> <span class="information d-block mt-5">Observers,SSO</span> <span class="information d-block mt-4">Dedicated,CSM</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    
+    
+</section>
+<br>
+    <br>
+    <section>
+
+    <div>
+        <!--END MENU BAR-->
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    
+        <a href="https://www.linkedin.com/in/om-gupta-291111198/"   target="_blank><i class="fa fa-university" style="font-size:20px;color:red"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://github.com/OMGUPTA21"  target="_blank>LINKS</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://www.facebook.com/pyomgupta21-100335458622051" target="_blank">YOUR FACBOOK PAGE</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://www.linkedin.com/in/om-gupta-291111198/" target="_blank">ABOUT ME</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://www.youtube.com/channel/UCBpgSleRY0JfzAyW59beChQ" target="_blank">CONTECT WITH YOUR CHANNEL</a>&nbsp;&nbsp;
+        <a href="#"> @COPYRIGHTT RESERVED</a>
+        
+        
+    </nav>
+
+  
+    </div>
+</section>
+
+    
+</body>
+</html>
