170c36aa2b8c624e0db27b65471da351b38e93e8
Om Gupta <73100677+OMGUPTA21@users.noreply.github.com>
2021-10-29 17:08:21 +0530

    Update and rename login.html to login.php

---
+++ login.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+     
+    </head>
+
+
+
+<style>
+.edu{
+    
+    width: 70px;
+}
+   body{
+        padding-top:4.2rem;
+		padding-bottom:4.2rem;
+		background:rgb(211, 206, 206);
+        }
+        a{
+         text-decoration:none !important;
+         }
+         h1,h2,h3{
+         font-family: 'Kaushan Script', cursive;
+         }
+          .myform{
+		position: relative;
+		display: -ms-flexbox;
+		display: flex;
+		padding: 1rem;
+		-ms-flex-direction: column;
+		flex-direction: column;
+		width: 100%;
+		pointer-events: auto;
+		background-color: #b0c0e4;
+		background-clip: padding-box;
+		border: 1px solid rgba(0,0,0,.2);
+		border-radius: 1.1rem;
+		outline: 0;
+		max-width: 500px;
+		 }
+         .tx-tfm{
+         text-transform:uppercase;
+         }
+         .mybtn{
+         border-radius:50px;
+         }
+        
+         .login-or {
+         position: relative;
+         color: #aaa;
+         margin-top: 10px;
+         margin-bottom: 10px;
+         padding-top: 10px;
+         padding-bottom: 10px;
+         }
+         .span-or {
+         display: block;
+         position: absolute;
+         left: 50%;
+         top: -2px;
+         margin-left: -25px;
+         background-color: #fff;
+         width: 50px;
+         text-align: center;
+         }
+         .hr-or {
+         height: 1px;
+         margin-top: 0px !important;
+         margin-bottom: 0px !important;
+         }
+         .google {
+         color:#666;
+         width:100%;
+         height:40px;
+         text-align:center;
+         outline:none;
+         border: 1px solid lightgrey;
+         }
+          form .error {
+         color: #ff0000;
+         }
+         #second{display:none;}
+	.log{
+	     color:white;
+	     baground-color: rgb(58, 55, 55);
+	}
+</style>
+
+
+
+
+<body>
+    <div class="container">
+        <div class="row">
+			<div class="col-md-5 mx-auto">
+			<div id="first">
+				<div class="myform form ">
+					 <div class="logo mb-3">
+						 <div class="col-md-12 text-center">
+							<center><img src="channels4_profile.jpg" alt="logo" class="edu"><b>&nbsp;GUPTA EDUCATIONAL HUB</b></center>
+							<h1>Login</h1>
+                        </div>              
+         
+ <div class="container mt-3">
+   <form action="Login.php" method="post">
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+   
+    
+    <button type="submit" class="btn btn-primary">SUBMIT AND CONTINUE</button><br><br><br>
+  
+  </form>
+ </div>
+ 
+ <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $email = $_POST['email'];
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Your email ' . $email.' has been submitted successfully!
+        
+      </div>';
+      echo ' <div class="alert alert-success alert-dismissible fade show" role="alert"><strong> we are happy to see you in our eduction portal</strong></div>';
+      // Submit these to a database
+    }
+    echo "<td>";
+    
+
+
+?>   
+<a href="index.html">CONTINUE</a><br><br><br>
+
+
+   
+			
+
+
+         
+</body>
+</html>
